23b49a8f68c6f4dbe6e809b29130d62db7e04cfd
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-05-13 17:41:57 +0530

    footer formated

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
             <a href="https://flowbite.com/" className="hover:underline">
-              Flowbite™
+              NG group™
             </a>
             . All Rights Reserved.
           </span>
@@ -14,21 +14,6 @@ const Footer = () => {
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6 ">
                 About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
               </a>
             </li>
           </ul>
