d94d0e14b75c7d15d5a1f86bf650b6944d0b06cb
Gouranga Satapathy <pulusatapathy@gmail.com>
2024-05-09 18:45:16 +0530

    refactor

--- src/components/test/Test.tsx
+++ src/components/test/Test.tsx
@@ -1,23 +1,14 @@
-import { mainPageObjProps } from "../mainPage/MainPage";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Test = () => {
     const [ipData, setIpData] = useState<Record<string, unknown>>({});
-
-    const [, setTestData] = useState<mainPageObjProps>();
     const loadSavedState = () => {
-        const savedState = localStorage.getItem("mainPageObj");
-        if (savedState) {
-            const parsedState: mainPageObjProps = JSON.parse(savedState);
-            setTestData(parsedState);
-        }
-
         axios.get("https://ipinfo.io/json").then((d) => {
             setIpData(d.data);
         });
     };
-
+    
     useEffect(() => {
         loadSavedState();
     }, []);
